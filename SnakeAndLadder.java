0eeedc0763796fbe13c7e4e5013dbbbb019744b7
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-29 22:41:42 +0530

    Added Code for startPosition

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.javaprogram.snakeandladder;
+
+import java.util.Random;
+
+public class SnakeAndLadder {
+	private static int startPosition=0;
+
+  	public static void main(String[] args) {
+
+	}
+}
