3e7f1c7512c20edce614df88b95826ff5c96f564
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-29 23:04:59 +0530

    Added code for random number generation

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,5 +7,10 @@ public class SnakeAndLadder {
 	private static int startPosition=0;
   	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder");
-	}
+		Random random=new Random();
+                int dieResult=random.nextInt(6)+1;
+		System.out.println("DieResult:"+dieResult);
+
+
+}
 }
