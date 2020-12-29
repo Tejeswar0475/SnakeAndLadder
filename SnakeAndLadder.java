054a7d33b4759a3bb05778f785896a883af46654
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-29 23:24:38 +0530

    Added code to check for options and implmentation if respective methods

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,12 +5,33 @@ import java.util.Random;
 public class SnakeAndLadder {
 
 	private static int startPosition=0;
+	private static int currentPosition;
+
   	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder");
 		Random random=new Random();
                 int dieResult=random.nextInt(6)+1;
-		System.out.println("DieResult:"+dieResult);
+		int checkForAOption=random.nextInt(3)+1;
+		switch(checkForAOption)
+                {
+                	case 1:  System.out.println("The player choose no play so current position "+currentPosition);
+                                 break;
+                        case 2:  ladder(dieResult);
+                                 break;
+                        default: snake(dieResult);
+                }
 
+	}
 
-}
+	private static void ladder(int dieResult) {
+                System.out.println("laddderMethod before - currentPosition:"+currentPosition);
+                currentPosition = currentPosition + dieResult;
+        	System.out.println("ladderMethod after - currentPosition:"+currentPosition);
+	}
+
+        private static void snake(int dieResult) {
+                System.out.println("SnakeMethod - currentPosition:"+currentPosition);
+                currentPosition = currentPosition - dieResult;
+		System.out.println("SnakeMethod after-currentPosition:"+currentPosition);
+        }
 }
