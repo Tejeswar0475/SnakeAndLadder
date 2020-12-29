27923d3d4b8190e38b7cdcf85b02767d1339e63b
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-29 22:59:34 +0530

    Added a varibale startPosition

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 
+	private static int startPosition=0;
   	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder");
 	}
