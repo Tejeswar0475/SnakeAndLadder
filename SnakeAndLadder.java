7866d0d92fc780ecc8f2230219256847ab8aed1c
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-29 22:54:43 +0530

    Added welcome message

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,9 +3,8 @@ package com.javaprogram.snakeandladder;
 import java.util.Random;
 
 public class SnakeAndLadder {
-	private static int startPosition=0;
 
   	public static void main(String[] args) {
-
+		System.out.println("Welcome to snake and ladder");
 	}
 }
