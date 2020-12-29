67096e815d35b363798b94b60f2962db1cbe0418
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-29 23:36:08 +0530

    Added code for a while loop and if statement in snake method

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,29 +9,37 @@ public class SnakeAndLadder {
 
   	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder");
-		Random random=new Random();
-                int dieResult=random.nextInt(6)+1;
-		int checkForAOption=random.nextInt(3)+1;
-		switch(checkForAOption)
-                {
-                	case 1:  System.out.println("The player choose no play so current position "+currentPosition);
-                                 break;
-                        case 2:  ladder(dieResult);
-                                 break;
-                        default: snake(dieResult);
-                }
+		while (currentPosition != 100 )
+		{
+			Random random=new Random();
+                	int dieResult=random.nextInt(6)+1;
+			int checkForAOption=random.nextInt(3)+1;
+			switch(checkForAOption)
+               		{
+                		case 1:  System.out.println("The player choose no play so current position "+currentPosition);
+                                	 break;
+                        	case 2:  ladder(dieResult);
+                                 	 break;
+                        	default: snake(dieResult);
+                	}
 
+		}
 	}
 
 	private static void ladder(int dieResult) {
                 System.out.println("laddderMethod before - currentPosition:"+currentPosition);
                 currentPosition = currentPosition + dieResult;
         	System.out.println("ladderMethod after - currentPosition:"+currentPosition);
+		
 	}
 
         private static void snake(int dieResult) {
                 System.out.println("SnakeMethod - currentPosition:"+currentPosition);
                 currentPosition = currentPosition - dieResult;
-		System.out.println("SnakeMethod after-currentPosition:"+currentPosition);
-        }
+		if(currentPosition <= 0)
+                {
+                        currentPosition=0;
+                }
+		System.out.println("snakeMethod:Current Position is "+currentPosition);
+	}
 }
